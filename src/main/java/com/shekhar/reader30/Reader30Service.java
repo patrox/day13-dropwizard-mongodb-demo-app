3651aad88f36e97f332db19d203101a0e0016264
Patryk <patryk.najda@wp.pl>
2015-03-03 13:42:44 +0100

    Heroku deployment

--- src/main/java/com/shekhar/reader30/Reader30Service.java
+++ src/main/java/com/shekhar/reader30/Reader30Service.java
@@ -1,5 +1,6 @@
 package com.shekhar.reader30;
 
+import com.mongodb.MongoURI;
 import net.vz.mongodb.jackson.JacksonDBCollection;
 
 import com.mongodb.DB;
@@ -13,12 +14,15 @@ import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.UnknownHostException;
 
 public class Reader30Service extends Application<Reader30Configuration> {
 
     public static void main(String[] args) throws Exception {
-        new Reader30Service().run(new String[] { "server", "src/main/resources/config.yml" });
+        new Reader30Service().run(new String[] { "server" });
     }
     
     @Override
@@ -34,9 +38,18 @@ public class Reader30Service extends Application<Reader30Configuration> {
 
     @Override
     public void run(Reader30Configuration configuration, Environment environment) throws MongoException, UnknownHostException {
-        Mongo mongo = new Mongo(configuration.mongohost, configuration.mongoport);
+        Mongo mongo;
+
+        if (System.getenv("MONGOLAB_URI") != null) {
+            MongoURI mongoURI = new MongoURI(System.getenv("MONGOLAB_URI"));
+            mongo = new Mongo(mongoURI);
+        }
+        else {
+            mongo = new Mongo(configuration.mongohost, configuration.mongoport);
+        }
+
         DB db = mongo.getDB(configuration.mongodb);
-        
+
         JacksonDBCollection<Blog, String> blogs = JacksonDBCollection.wrap(db.getCollection("blogs"), Blog.class, String.class);
         MongoManaged mongoManaged = new MongoManaged(mongo);
         environment.lifecycle().manage(mongoManaged);
